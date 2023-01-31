2606ff0012b180631327f7d65af0fd02c0624668
vishwesh91 <vishwesh@pi314.in>
2023-01-31 08:41:26 +0530

    static image optimization added

--- next.config.js
+++ next.config.js
@@ -1,11 +1,19 @@
 /** @type {import('next').NextConfig} */
+const withExportImages = require('next-export-optimize-images')
+
 const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
   images: {
     domains: ['localhost', 'api.myforexbuddy.com'],
-    unoptimized: true
   }
 }
 
-module.exports = nextConfig
+
+
+module.exports = (_phase, { defaultConfig }) => {
+  const plugins = [withExportImages]
+  return plugins.reduce((acc, plugin) => plugin(acc), { ...defaultConfig, ...nextConfig })
+}
+
+
