94142de1513095e3510ed3e378f5ae4472715057
vishwesh91 <vishwesh@pi314.in>
2023-04-01 03:41:40 +0530

    navbar optimised

--- next.config.js
+++ next.config.js
@@ -1,22 +1,24 @@
 /** @type {import('next').NextConfig} */
-const withExportImages = require("next-export-optimize-images");
+// const withExportImages = require("next-export-optimize-images");
 
 const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
   images: {
     domains: ["localhost", "api.myforexbuddy.com", "images.unsplash.com"],
+    unoptimized:true
   },
   swcMinify: true,
   
 };
 
 
+module.exports = nextConfig
 
-module.exports = (_phase, { defaultConfig }) => {
-  const plugins = [withExportImages];
-  return plugins.reduce((acc, plugin) => plugin(acc), {
-    ...defaultConfig,
-    ...nextConfig,
-  });
-};
+// module.exports = (_phase, { defaultConfig }) => {
+//   const plugins = [withExportImages];
+//   return plugins.reduce((acc, plugin) => plugin(acc), {
+//     ...defaultConfig,
+//     ...nextConfig,
+//   });
+// };
